0de3a57e3ee933ee1c810caaefc77584ba909121
SASWAT-KUNAL <pandasaswat.22@gmail.com>
2024-09-19 22:28:36 +0530

    Worked on config.py,data_ingestion.py,data_preprocessing.py

---
+++ src/data_ingestion.py
@@ -0,0 +1,10 @@
+import pandas as pd
+from config import Config
+
+class Dataloader:
+    def __init__(self):
+        self.raw_data_path = Config.RAW_DATA_PATH
+
+
+    def load_raw_data(self):
+        return pd.read_csv(self.raw_data_path)
